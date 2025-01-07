ed059f968c2487a6353df595b21f7a057b85f189
Ganesh Kasture <ganeshkasture9595@gmail.com>
2025-01-07 17:14:42 +0530

    Hero And About Section completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from "./components/About"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+  return (
+    <main className="bg-zinc-700 relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar/>
+      <Hero/>
+      <About/>
+    </main>
+  )
+}
+
+export default App
